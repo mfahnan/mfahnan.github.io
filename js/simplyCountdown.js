364edaaf5a12a33fa85f4b60cee047e4fb7565e2
xnaan <ahnanm06@gmail.com>
2024-10-04 00:39:52 +0700

    up

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -111,10 +111,10 @@
     exports.simplyCountdown = (elt, args) => {
         const eltProto = Object.getPrototypeOf(elt);
         let parameters = extend({
-            year: 2015,
+            year: 2025,
             month: 6,
             day: 28,
-            hours: 9,
+            hours: 8,
             minutes: 0,
             seconds: 0,
             words: {
